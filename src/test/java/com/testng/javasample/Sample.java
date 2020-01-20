2130145fe858f6e6833b0c6951c1ecbf07dd4a67
pallav <Naveen Nani@DESKTOP-0T2OPP2>
2020-01-20 22:11:53 +0530

    Sample.java Updated

--- src/test/java/com/testng/javasample/Sample.java
+++ src/test/java/com/testng/javasample/Sample.java
@@ -2,23 +2,45 @@ package com.testng.javasample;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Sample {
 	
-	@Test()
+	WebDriver driver=null;
+	
+	@BeforeTest
+	
+	public void beforeTest() {
+		WebDriverManager.chromedriver().setup();
+		 driver=new ChromeDriver();
+		
+		
+	}
+	
+	@Test(/*dataProvider="dataProvider"*/)
 	public void test() throws InterruptedException
 	{
-		/*String path="C:\\Users\\Naveen Nani\\Downloads\\chromedriver_win32\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver", path);
-		*/
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		
 		driver.get("https://www.google.com/");
 		Thread.sleep(1000);
 		System.out.print("driver.getTitle()"+driver.getTitle());
 	}
+	@AfterTest
+	public void afterTest() {
+		
+		driver.close();
+		driver.quit();
+		
+	}
+	/*@DataProvider
+	public Object [][] dataProvider(){
+		Object obj =new Object();
+		return (Object[][]) obj;
+	}*/
 
 }
